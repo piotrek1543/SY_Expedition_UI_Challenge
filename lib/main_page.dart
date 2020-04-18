de45dbd0836dbe0051336bc37e4b05ac93296b7f
Piotr <piotrekert90@gmail.com>
2020-04-18 08:52:06 +0200

    Add PageOffsetNotifier, LeopardPage and VulturePage.
    Note:
    + PageOffsetNotifier tells about PageView actual positions, where this widget was moved. It would be needed to implement background image of PageView.
    + LeopardPage and VulturePage have temporary implementation.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
+class PageOffsetNotifier with ChangeNotifier {
+  double _offset = 0;
+  double _page = 0;
+
+  PageOffsetNotifier(PageController pageController) {
+    pageController.addListener(() {
+      _offset = pageController.offset;
+      _page = pageController.page;
+      notifyListeners();
+    });
+  }
+
+  double get offset => _offset;
+
+  double get page => _page;
+}
+
 class MainPage extends StatelessWidget {
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Hello')),
+      body: Stack(
+        children: <Widget>[
+          PageView(
+            controller: _pageController,
+            physics: ClampingScrollPhysics(),
+            children: <Widget>[
+              LeopardPage(),
+              VulturePage(),
+            ],
+          ),
+          Image.asset('assets/leopard.png'),
+        ],
+      ),
     );
+  }
+}
+
+class LeopardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/leopard.png');
+  }
+}
+
+class VulturePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/vulture.png');
   }
 }
