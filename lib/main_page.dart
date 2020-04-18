c7fd0a9316d5c56d5edc45e0e36954751be9bd6a
Piotr <piotrekert90@gmail.com>
2020-04-18 09:12:00 +0200

    Add LeopardImage widget as background. Set MainPage StatefulWidget.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PageOffsetNotifier with ChangeNotifier {
   double _offset = 0;
@@ -17,26 +18,48 @@ class PageOffsetNotifier with ChangeNotifier {
   double get page => _page;
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
   final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          PageView(
-            controller: _pageController,
-            physics: ClampingScrollPhysics(),
-            children: <Widget>[
-              LeopardPage(),
-              VulturePage(),
-            ],
-          ),
-          Image.asset('assets/leopard.png'),
-        ],
+    return ChangeNotifierProvider(
+      builder: (_) => PageOffsetNotifier(_pageController),
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            PageView(
+              controller: _pageController,
+              physics: ClampingScrollPhysics(),
+              children: <Widget>[
+                LeopardPage(),
+                VulturePage(),
+              ],
+            ),
+            LeopardImage(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class LeopardImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PageOffsetNotifier>(builder: (context, notifier, child) {
+      return Positioned(
+        top: 100,
+        left: -notifier.offset,
+        width: MediaQuery.of(context).size.width * 1.5,
+        child: Image.asset('assets/leopard.png'),
+      );
+    });
   }
 }
 
