14fe06f6cfc457151657b6b1b3b666ed1919cd8a
Piotr <piotrekert90@gmail.com>
2020-04-19 08:55:07 +0200

    Move leopardPage related code to separate file.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'constants.dart';
+import 'leopard_page.dart';
 
 class PageOffsetNotifier with ChangeNotifier {
   double _offset = 0;
@@ -61,24 +62,6 @@ class _MainPageState extends State<MainPage> {
   }
 }
 
-class LeopardImage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<PageOffsetNotifier>(
-      builder: (context, notifier, child) {
-        return Positioned(
-          left: -0.85 * notifier.offset,
-          width: MediaQuery.of(context).size.width * 1.6,
-          child: child,
-        );
-      },
-      child: IgnorePointer(
-        child: Image.asset('assets/leopard.png'),
-      ),
-    );
-  }
-}
-
 class VultureImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -128,23 +111,6 @@ class AppBar extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class LeopardPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        SizedBox(height: 128),
-        The72Text(),
-        SizedBox(height: 32),
-        TravelDescriptionLabel(),
-        SizedBox(height: 32),
-        LeopardDescription(),
-      ],
     );
   }
 }
@@ -204,27 +170,6 @@ class PageIndicator extends StatelessWidget {
   }
 }
 
-class TravelDescriptionLabel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<PageOffsetNotifier>(
-      builder: (context, notifier, child) {
-        return Opacity(
-          opacity: math.max(0, 1 - 4 * notifier.page),
-          child: child,
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.only(left: 24),
-        child: Text(
-          'Travel description',
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
-    );
-  }
-}
-
 class ShareButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -232,59 +177,6 @@ class ShareButton extends StatelessWidget {
       right: 24,
       bottom: 16,
       child: Icon(Icons.share),
-    );
-  }
-}
-
-class LeopardDescription extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<PageOffsetNotifier>(
-      builder: (context, notifier, child) {
-        return Opacity(
-          opacity: math.max(0, 1 - 4 * notifier.page),
-          child: child,
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Text(
-          'The leopard is distinguished by its well-camouflaged fur, opportunistic hunting behaviour, broad diet, and strength.',
-          style: TextStyle(fontSize: 13, color: lightGrey),
-        ),
-      ),
-    );
-  }
-}
-
-class The72Text extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<PageOffsetNotifier>(
-      builder: (context, notifier, child) {
-        return Transform.translate(
-          offset: Offset(-48 - 0.5 * notifier.offset, 0),
-          child: child,
-        );
-      },
-      child: Container(
-        alignment: Alignment.topLeft,
-        child: RotatedBox(
-          quarterTurns: 1,
-          child: Container(
-            width: 400,
-            child: FittedBox(
-              fit: BoxFit.cover,
-              child: Text(
-                '72',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
