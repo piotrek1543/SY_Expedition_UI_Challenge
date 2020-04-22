98f63ea0f434a27f8f3e12d87932dad3821fc6eb
Piotr <piotrekert90@gmail.com>
2020-04-22 20:46:54 +0200

    Move widgets to separate files.

---
+++ lib/widgets/share_button.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ShareButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 24,
+      bottom: 16,
+      child: Icon(Icons.share),
+    );
+  }
+}
---
+++ lib/widgets/start_camp_label.dart
@@ -0,0 +1,36 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:syexpedition/notifiers/page_offset_notifier.dart';
+import 'package:syexpedition/position_helpers.dart';
+import 'package:syexpedition/widgets/map_hider.dart';
+
+class StartCampLabel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PageOffsetNotifier>(
+      builder: (context, notifier, child) {
+        double opacity = math.max(0, 4 * notifier.page - 3);
+        return Positioned(
+          top: topMargin(context) + mainSquareSize(context) + 32 + 16 + 32,
+          width: (MediaQuery.of(context).size.width - 48) / 3,
+          left: opacity * 24.0,
+          child: Opacity(
+            opacity: opacity,
+            child: child,
+          ),
+        );
+      },
+      child: MapHider(
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            'Start camp',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+          ),
+        ),
+      ),
+    );
+  }
+}
