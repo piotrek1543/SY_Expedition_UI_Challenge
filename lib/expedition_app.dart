0c183b81318c086c859631af486183bd10b3a1cd
Piotr <piotrekert90@gmail.com>
2020-04-22 18:34:57 +0200

    Create new `screens` directory. Move main_page.dart.

--- lib/expedition_app.dart
+++ lib/expedition_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syexpedition/styles/styles.dart';
-import 'package:syexpedition/main_page.dart';
+import 'package:syexpedition/screens/main_page.dart';
 
 class ExpeditionApp extends StatelessWidget {
   // This widget is the root of your application.
