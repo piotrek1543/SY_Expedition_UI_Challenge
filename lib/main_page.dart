a6ab082eeebdbfd968e2562afa7399d4e29815e2
Piotr <piotrekert90@gmail.com>
2020-04-22 09:28:55 +0200

    Add MapBaseCamp label and MapLeopards icon.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -122,6 +122,8 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                         VultureIconLabel(),
                         LeopardIconLabel(),
                         CurvedRoute(),
+                        MapBaseCamp(),
+                        MapLeopards(),
                       ],
                     ),
                   ),
@@ -1021,6 +1023,65 @@ class SmallAnimalIconLabel extends StatelessWidget {
             color: white,
           ),
       ],
+    );
+  }
+}
+
+class MapBaseCamp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MapAnimationNotifier>(
+      builder: (context, notifier, child) {
+        double opacity = math.max(0, 4 * (notifier.value - 3 / 4));
+        return Positioned(
+          top: 128.0 + 32 + 16 + 4,
+          width: (MediaQuery.of(context).size.width - 48) / 3,
+          right: 30.0,
+          child: Opacity(
+            opacity: opacity,
+            child: child,
+          ),
+        );
+      },
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          'Base camp',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class MapLeopards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MapAnimationNotifier>(
+      builder: (context, notifier, child) {
+                double startTop = 128.0 + 400 + 32 + 16 + 32 + 4;
+        double endTop = 128.0 + 32 + 16 + 8;
+        double oneThird = (startTop - endTop) / 3;
+        double opacity = math.max(0, 4 * (notifier.value - 3 / 4));
+        return Positioned(
+          top: 128 + 32 + 16 + 4 + oneThird,
+          child: Opacity(
+            opacity: opacity,
+            child: child,
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: SmallAnimalIconLabel(
+          isVulture: false,
+          showLine: false,
+        ),
+      ),
     );
   }
 }
